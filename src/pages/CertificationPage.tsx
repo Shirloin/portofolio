bd44f6a1f19d0b5b64d1d44eb5f51019f6904657
Shirloin <riccardo@binus.ac.id>
2024-09-23 23:08:04 +0700

    fix: image in certificate change to static

--- src/pages/CertificationPage.tsx
+++ src/pages/CertificationPage.tsx
@@ -4,30 +4,26 @@ import { ICertificate } from "../interfaces/ICertificate";
 const certificateList: ICertificate[] = [
   {
     link: "https://coursera.org/share/4495e44a9580a23bffa33309358b627f",
-    image:
-      "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~2RSF9XCQVCZP/CERTIFICATE_LANDING_PAGE~2RSF9XCQVCZP.jpeg",
+    image: "/images/certificate/IntroToAndroidMobileAppDev.jpeg",
   },
   {
     link: "https://coursera.org/share/dc52f8355cd5eab82a1c0f87852a2fc9",
-    image:
-      "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~D9K4XUX5ZKBK/CERTIFICATE_LANDING_PAGE~D9K4XUX5ZKBK.jpeg",
+    image: "/images/certificate/VersionControl.jpeg",
   },
   {
     link: "https://coursera.org/share/2a605325923c2437edbf1fb0d5321e41",
-    image:
-      "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~WSQN255ZJNZW/CERTIFICATE_LANDING_PAGE~WSQN255ZJNZW.jpeg",
+    image: "/images/certificate/PrinciplesOfUXUIDesign.jpeg",
   },
   {
     link: "https://coursera.org/share/d5f0379703666c87d09c82624267415c",
-    image:
-      "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~58BCMWONZXIV/CERTIFICATE_LANDING_PAGE~58BCMWONZXIV.jpeg",
+    image: "/images/certificate/ProgrammingFundamentalsInKotlin.jpeg",
   },
 ];
 
 export default function CertificationPage() {
   return (
     <>
-      <div className="mx-auto my-20 overflow-hidden">
+      <div className="mx-auto my-20 flex flex-col justify-start overflow-hidden">
         <h1 className="mb-4 text-center text-base font-bold md:mb-10 md:text-3xl">
           Certification Lists
         </h1>
