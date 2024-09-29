c5d720374aa3980ae490c6a279df561fb1447ac9
Shirloin <riccardo@binus.ac.id>
2024-09-29 22:24:28 +0700

    feat: animation on menu sheet

--- src/components/MenuSheet.tsx
+++ src/components/MenuSheet.tsx
@@ -11,52 +11,50 @@ export default function MenuSheet() {
       >
         <i className="bi bi-list"></i>
       </button>
-      {isOpen && (
-        <div
-          className={`absolute inset-0 h-screen overflow-hidden bg-white p-4 font-bold transition-all duration-300 ${isOpen ? "translate-x-full" : "translate-x-0"}`}
-        >
-          <div className="flex h-full flex-col">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="flex h-7 w-7 items-center justify-center self-end rounded-md border-2 border-black text-center text-black"
-            >
-              <i className="fa fa-close"></i>
-            </button>
-            <Link
-              to={"/"}
-              onClick={() => setIsOpen(!isOpen)}
-              className="my-2 p-2 text-center text-xl"
-            >
-              Home
-            </Link>
-            <hr />
-            <Link
-              to={"/project"}
-              onClick={() => setIsOpen(!isOpen)}
-              className="my-2 p-2 text-center text-xl"
-            >
-              Projects
-            </Link>
-            <hr />
-            <Link
-              to={"/certification"}
-              onClick={() => setIsOpen(!isOpen)}
-              className="my-2 p-2 text-center text-xl"
-            >
-              Certifications
-            </Link>
-            <hr />
-            <Link
-              to={"/contact"}
-              onClick={() => setIsOpen(!isOpen)}
-              className="my-2 p-2 text-center text-xl"
-            >
-              Contact
-            </Link>
-            <hr />
-          </div>
+      <div
+        className={`animate__animated absolute inset-0 h-screen overflow-hidden bg-white p-4 font-bold transition-all duration-300 ${isOpen ? "animate__fadeInRight" : "animate__fadeOutRight opacity-0"}`}
+      >
+        <div className="flex h-full flex-col">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="flex h-7 w-7 items-center justify-center self-end rounded-md border-2 border-black text-center text-black"
+          >
+            <i className="fa fa-close"></i>
+          </button>
+          <Link
+            to={"/"}
+            onClick={() => setIsOpen(!isOpen)}
+            className="my-2 p-2 text-center text-xl"
+          >
+            Home
+          </Link>
+          <hr />
+          <Link
+            to={"/project"}
+            onClick={() => setIsOpen(!isOpen)}
+            className="my-2 p-2 text-center text-xl"
+          >
+            Projects
+          </Link>
+          <hr />
+          <Link
+            to={"/certification"}
+            onClick={() => setIsOpen(!isOpen)}
+            className="my-2 p-2 text-center text-xl"
+          >
+            Certifications
+          </Link>
+          <hr />
+          <Link
+            to={"/contact"}
+            onClick={() => setIsOpen(!isOpen)}
+            className="my-2 p-2 text-center text-xl"
+          >
+            Contact
+          </Link>
+          <hr />
         </div>
-      )}
+      </div>
     </>
   );
 }
