229d89426834c04c78f49c8b60413e946771415a
Shirloin <riccardo@binus.ac.id>
2024-12-12 13:01:04 +0700

    fix: certification card

--- src/components/cards/CertificationCard.tsx
+++ src/components/cards/CertificationCard.tsx
@@ -10,13 +10,13 @@ export default function CertificationCard({
   return (
     <>
       <a
-        className="animate__animated animate__zoomIn max-w-xl"
+        className="animate__animated animate__zoomIn h-[380px] max-w-[500px]"
         href={certificate.link || undefined}
         target={certificate.link ? "_blank" : undefined}
       >
         <img
           id="cardHover"
-          className="h-full w-full rounded-lg border object-contain"
+          className="min-h-full min-w-full rounded-lg border object-contain"
           src={certificate.image}
           alt=""
           loading="lazy"
